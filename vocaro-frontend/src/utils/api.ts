c39411bb20871ee29bf24f2b06008798118ac813
Muqaddas <muqaddassatti708@gmail.com>
2026-05-09 11:56:44 +0500

    Update backend URL to HF Space

--- vocaro-frontend/src/utils/api.ts
+++ vocaro-frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Backend ka address
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://muqaddas7-vocaro-backend.hf.space";
 
 export const api = axios.create({
   baseURL: BASE_URL,
